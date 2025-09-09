601dcfe66646b7d9c2565bcbbef6967241cdf601
nguyenanhquan <quan715105194@gmail.com>
2025-09-09 10:44:49 +0700

    crawler data info product glamira

--- src/pid_fetch.py
+++ src/pid_fetch.py
@@ -1,3 +1,5 @@
+import time
+
 import pandas as pd
 from config.connect import get_mongo_client, get_collection, get_collection_name, MongoConfig
 from config.dir.address import uri, db_name, collection, domains
@@ -9,17 +11,40 @@ cfg = MongoConfig(
 
 client = get_mongo_client(cfg)
 collection = get_collection_name(client, cfg.db_name, collection)
+print("✅ Kết nối thành công!")
 
 batch_size = 100000
 
-cursor = collection.find({}, {"product_id": 1, "_id": 0})
-product_ids = [doc["product_id"] for doc in cursor if "product_id" in doc]
+target_collections = [
+    "view_product_detail", "select_product_option", "select_product_option_quality",
+    "add_to_cart_action", "product_detail_recommendation_visible", "product_detail_recommendation_noticed",
+    "product_view_all_recommend_clicked"
+]
 
-df = pd.DataFrame(product_ids, columns=["product_id"]).drop_duplicates()
+query = {
+    "collection": {"$in": target_collections}
+}
+
+projection = {
+    "collection": 1,
+    "product_id": 1,
+    "viewing_product_id": 1,
+    "_id": 0
+}
+print("🔍 Đang truy vấn dữ liệu từ collection 'summary'...")
+cursor = collection.find(query, projection)
+product_ids = set()
+for doc in cursor:
+    pid = doc.get("product_id") or doc.get("viewing_product_id")
+    if pid:
+        product_ids.add(pid)
+
+df = pd.DataFrame(list(product_ids), columns=["product_id"])
 
 chunk_size = 3500
 for i, start in enumerate(range(0, len(df), chunk_size), start=1):
     chunk = df.iloc[start:start + chunk_size]
-    chunk.to_csv(f"node{i}.csv", index=False)
+    chunk.to_csv(f"pid{i}.csv", index=False)
+    time.sleep(0.5)
 
-print(f"Unique product_ids saved to {((len(df) - 1) // chunk_size) + 1} files.")
+print(f"Đã lưu {((len(df) - 1) // chunk_size) + 1} file CSV chứa {len(df)} product_id duy nhất.")
