c19e0b4f936860c344609732ca2eb20fcfd555c5
nguyenanhquan <quan715105194@gmail.com>
2025-09-16 09:35:08 +0700

    import data glamira prdinfo crawling to mongodb

--- src/pid_fetch.py
+++ src/pid_fetch.py
@@ -1,50 +1,57 @@
+import logging
 import time
 
 import pandas as pd
 from config.connect import get_mongo_client, get_collection, get_collection_name, MongoConfig
 from config.dir.address import uri, db_name, collection, domains
+from logs.config_logs import setup_logging
 
-cfg = MongoConfig(
-    uri=uri,
-    db_name=db_name
-)
+setup_logging()
 
-client = get_mongo_client(cfg)
-collection = get_collection_name(client, cfg.db_name, collection)
-print("Kết nối thành công!")
+try:
+    cfg = MongoConfig(
+        uri=uri,
+        db_name=db_name
+    )
 
-batch_size = 100000
+    client = get_mongo_client(cfg)
+    collection = get_collection_name(client, cfg.db_name, collection)
+    logging.info("Connected to mongodb")
 
-target_collections = [
-    "view_product_detail", "select_product_option", "select_product_option_quality",
-    "add_to_cart_action", "product_detail_recommendation_visible", "product_detail_recommendation_noticed",
-    "product_view_all_recommend_clicked"
-]
+    batch_size = 100000
 
-query = {
-    "collection": {"$in": target_collections}
-}
+    target_collections = [
+        "view_product_detail", "select_product_option", "select_product_option_quality",
+        "add_to_cart_action", "product_detail_recommendation_visible", "product_detail_recommendation_noticed",
+        "product_view_all_recommend_clicked"
+    ]
 
-projection = {
-    "collection": 1,
-    "product_id": 1,
-    "viewing_product_id": 1,
-    "_id": 0
-}
-print("🔍 Đang truy vấn dữ liệu từ collection 'summary'...")
-cursor = collection.find(query, projection)
-product_ids = set()
-for doc in cursor:
-    pid = doc.get("product_id") or doc.get("viewing_product_id")
-    if pid:
-        product_ids.add(pid)
+    query = {
+        "collection": {"$in": target_collections}
+    }
 
-df = pd.DataFrame(list(product_ids), columns=["product_id"])
+    projection = {
+        "collection": 1,
+        "product_id": 1,
+        "viewing_product_id": 1,
+        "_id": 0
+    }
+    print("🔍 Đang truy vấn dữ liệu từ collection 'summary'...")
+    cursor = collection.find(query, projection)
+    product_ids = set()
+    for doc in cursor:
+        pid = doc.get("product_id") or doc.get("viewing_product_id")
+        if pid:
+            product_ids.add(pid)
 
-chunk_size = 3500
-for i, start in enumerate(range(0, len(df), chunk_size), start=1):
-    chunk = df.iloc[start:start + chunk_size]
-    chunk.to_csv(f"pid{i}.csv", index=False)
-    time.sleep(0.5)
+    df = pd.DataFrame(list(product_ids), columns=["product_id"])
 
-print(f"Đã lưu {((len(df) - 1) // chunk_size) + 1} file CSV chứa {len(df)} product_id duy nhất.")
+    chunk_size = 3500
+    for i, start in enumerate(range(0, len(df), chunk_size), start=1):
+        chunk = df.iloc[start:start + chunk_size]
+        chunk.to_csv(f"pid{i}.csv", index=False)
+        time.sleep(0.5)
+
+    print(f"Đã lưu {((len(df) - 1) // chunk_size) + 1} file CSV chứa {len(df)} product_id duy nhất.")
+except Exception as e:
+    logging.error(e)
---
+++ src/products_unique.py
@@ -0,0 +1,40 @@
+import logging
+
+from logs.config_logs import setup_logging
+from config.connect import MongoConfig, get_mongo_client, get_collection_name
+from config.dir.address import uri, db_name, collection_products, collection_prdinfo
+setup_logging()
+
+try:
+    cfg = MongoConfig(
+        uri=uri,
+        db_name=db_name,
+    )
+    client = get_mongo_client(cfg)
+    collection_old = get_collection_name(client, cfg.db_name, collection_prdinfo)
+    collection = get_collection_name(client, cfg.db_name, collection_products)
+    logging.info("Connected to mongodb successfully!")
+    pipeline = [
+        {
+            "$match": {
+                "title": {"$regex": "^[A-Za-z0-9 ,.!?\"'-]+$"}
+            }
+        },
+        {
+            "$group": {
+                "_id": "$product_id",
+                "doc": {"$first": "$$ROOT"}
+            }
+        },
+        {
+            "$replaceRoot": {"newRoot": "$doc"}
+        }
+    ]
+    result = list(collection_old.aggregate(pipeline))
+    if result:
+        collection.insert_many(result)
+        print(f"Đã insert {len(result)} sản phẩm vào 'products'")
+    else:
+        logging.error("Insert failed!")
+except Exception as e:
+    logging.error("Error connect DB products".format(e))
