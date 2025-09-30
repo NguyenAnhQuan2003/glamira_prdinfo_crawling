42c949dee74e7e8a0da57da2e986d286d93209cf
nguyenanhquan <quan715105194@gmail.com>
2025-09-30 09:15:49 +0700

    update upload summary.jsonl to GCS

---
+++ src/glamira_summary_header.py
@@ -0,0 +1,26 @@
+import json
+import logging
+
+from config.dir.address import output_dir_jsonl, data_header
+from logs.config_logs import setup_logging
+setup_logging()
+
+
+input_file = output_dir_jsonl
+output_file = data_header
+
+all_fields = set()
+
+with open(input_file, "r", encoding="utf-8") as infile:
+    for i, line in enumerate(infile, 1):
+        try:
+            data = json.loads(line.strip())
+            all_fields.update(data.keys())
+        except json.JSONDecodeError as e:
+            logging.error("Failed to parse ling {}".format(e))
+            continue
+
+with open(output_file, "w", encoding="utf-8") as outfile:
+    json.dump(list(all_fields), outfile, indent=2)
+
+print(f"Header đã được xuất ra file: {output_file}")
