e646c6986fae1fbf1ac9580f5eb654d88babf45c
nguyenanhquan <quan715105194@gmail.com>
2025-09-30 09:42:04 +0700

    update upload summary.jsonl to GCS

--- src/glamira_summary_header.py
+++ src/glamira_summary_header.py
@@ -1,6 +1,6 @@
 import json
 import logging
-
+import os
 from config.dir.address import data_summary_jsonl, data_header
 from logs.config_logs import setup_logging
 setup_logging()
@@ -10,7 +10,7 @@ input_file = data_summary_jsonl
 output_file = data_header
 
 all_fields = set()
-
+os.makedirs(data_header, exist_ok=True)
 with open(input_file, "r", encoding="utf-8") as infile:
     for i, line in enumerate(infile, 1):
         try:
