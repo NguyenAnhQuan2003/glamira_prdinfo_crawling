ccea78e845a302845295cf53263bb18fb83b0b4d
nguyenanhquan <quan715105194@gmail.com>
2025-10-05 00:53:27 +0700

    update transform data

---
+++ glamira_dbt/models/analysis_looker/analysis_gender.sql
@@ -0,0 +1,18 @@
+WITH checkSuccess AS (
+    SELECT
+        product_key
+    FROM
+        {{source('fact_glamira', 'fact_sales_order')}}
+),
+joinProduct AS (
+    SELECT 
+        cs.product_key AS product_key,
+        dp.product_gender AS product_gender
+    FROM checkSuccess cs
+    INNER JOIN {{source('dim_glamira', 'dim_products')}} dp
+    ON dp.product_key = cs.product_key
+)
+SELECT 
+    product_key,
+    product_gender
+FROM joinProduct
